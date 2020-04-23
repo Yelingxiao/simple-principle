34478353d663157382218b9ec64a865209137069
yelingxiao <1003772257@qq.com>
2020-04-23 14:35:53 +0800

    add: vite

---
+++ vue-vite/Comp.vue
@@ -0,0 +1,13 @@
+<template>
+  <button @click="count++">{{ count }}</button>
+</template>
+
+<script>
+export default {
+  data: () => ({ count: 2 })
+}
+</script>
+
+<style scoped>
+button { color: red }
+</style>
